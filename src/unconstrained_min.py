5f05d3b007ebd17d40b672a2dfdc628660418f62
Oron Paz <oron.paz26@gmail.com>
2025-05-25 14:50:46 +0300

    setting up the class

--- src/unconstrained_min.py
+++ src/unconstrained_min.py
@@ -1,2 +1,19 @@
-def minimize(f, x0, obj_tol, param_tol, max_iter):
-    pass
+class minimizationAlgorithms:
+    def __init__(self, algorithm):
+        self.algorithm = algorithm # either gradient descent OR newton search directions
+        self.history = []
+        
+    def minimize(self, f, x0, obj_tol, param_tol, max_iter):
+        if self.algorithm == 'Gradient Descent':
+            self.gradient_descent(f, x0, obj_tol, param_tol, max_iter)
+        elif self.algorithm == 'Newton Search Directions':
+            self.newton_search_directions(f, x0, obj_tol, param_tol, max_iter)
+        else:
+            print("Invalid Selection")
+
+    
+    def gradient_descent(self, f, x0, obj_tol, param_tol, max_iter):
+        pass
+
+    def newton_search_directions(self, f, x0, obj_tol, param_tol, max_iter):
+        pass
